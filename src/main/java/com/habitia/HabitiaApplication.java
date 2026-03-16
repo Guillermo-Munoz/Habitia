08a7ae7b9d159339845236a1539b83a00247da44
Guillermo-Munoz <illloquehaces@hotmail.com>
2026-03-16 08:40:55 +0100

    Initial commit

---
+++ src/main/java/com/habitia/HabitiaApplication.java
@@ -0,0 +1,13 @@
+package com.habitia.habitia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitia/habitia/HabitiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitia.habitia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
