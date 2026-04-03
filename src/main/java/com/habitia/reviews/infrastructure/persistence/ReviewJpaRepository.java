731477986f1304eb8b52d06648b22b516cc3209f
Guillermo-Munoz <illloquehaces@hotmail.com>
2026-04-03 12:28:04 +0200

    feat: add reviews persistence layer

---
+++ src/main/java/com/habitia/reviews/infrastructure/persistence/ReviewJpaRepository.java
@@ -0,0 +1,19 @@
+package com.habitia.reviews.infrastructure.persistence;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ReviewJpaRepository extends JpaRepository<ReviewJpaEntity, UUID> {
+
+    List<ReviewJpaEntity> findByBookingId(UUID bookingId);
+    
+    @Query("SELECT r FROM ReviewEntity r" +
+           "JOIN BookingJpaEntity b ON r.bookingId = b.id" +
+           "WHERE b.roomId = :roomId")
+    List<ReviewJpaEntity> findByRoomId(@Param("roomId") UUID roomId);
+
+} 
